d9786f3561a39e53f69deec5e8e4f808d7b66e86
Erick Fco <erick.barreto@colimasoft.com>
2022-06-22 16:39:30 -0500

    Migracion codigo ambiente back

---
+++ notaria/DataEntities/EstatusEntity.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace notaria.DataEntities
+{
+    public class EstatusEntity
+    {
+        public int id { get; set; }
+        [StringLength(50)]
+        public string? estatus { get; set; }
+    }
+}
---
+++ notaria/DataEntities/TramiteEntity.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace notaria.DataEntities
+{
+    public class TramiteEntity
+    {
+        public int id { get; set; }
+        public string? numeroDeEscritura { get; set; }
+        public DateTime? fechaCreacion { get; set; }
+        public DateTime? fechaEscritura { get; set; }
+        public DateTime? fechaFirma { get; set; }
+        [StringLength(100)]
+        public string? parteA { get; set; }
+        [StringLength(100)]
+        public string? parteB { get; set; }
+        public int? diasHabiles { get; set; }
+        //Foreing Key
+        public int? usuarioId { get; set; }
+        public int? estatusId { get; set; }
+        public int? actoId { get; set; }
+
+
+        [ForeignKey("usuarioId")]
+        public virtual UserEntity UserEntity { get; set; }
+
+        [ForeignKey("estatusId")]
+        public virtual EstatusEntity EstatusEntity { get; set; }
+
+        [ForeignKey("actoId")]
+        public virtual ActoEntity ActoEntity { get; set; }
+    }
+}
